d31af2fddeca9a124f54dd97d40aab1df2bc992a
Juno2703 <111256805+Juno2703@users.noreply.github.com>
2022-10-21 11:51:48 +0700

    first commit

--- appserver.js
+++ appserver.js
@@ -4,24 +4,6 @@ const appServer = express();
 const router = express.Router();
 const fs = require("fs");
 const path = require('path');
-//session 
-const cookieParser = require("cookie-parser");
-const sessions = require('express-session');
-const oneDay = 1000 * 60 * 60 * 24;
-appServer.use(sessions({
-    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-    saveUninitialized:true,
-    cookie: { maxAge: oneDay, 
-            TenTK: "",
-            MatKhau: ""},
-    resave: false 
-}));
-appServer.use(express.json());
-appServer.use(express.urlencoded({ extended: true }));
-
-
-appServer.use(express.static(__dirname));
-appServer.use(cookieParser());
 
 // Middleware
 const bodyParser = require("body-parser");
@@ -38,8 +20,8 @@ appServer.use(express.static("public"));
 const mongoose = require('mongoose');
 
 const accUN = "user202210";
-const accPW = "g51leZHfXqsJKH4f";
-const dnsServer = "cluster0.azn5qeo.mongodb.net";
+const accPW = "d3Z2x7JQEzUcdeDJ";
+const dnsServer = "cluster0.s7ukh.mongodb.net";
 const dbName = "Shop_2022_10";
 
 const uri = "mongodb+srv://"
@@ -65,21 +47,17 @@ router.use( (loixayra, yeucau, trave, ketiep) => {
 
 // ------------------- Routing
 router.get( "/" , (yeucau, trave) => {
-    trave.render("userviewhomepage", {TenTrang: "Main Chinh !!!"} );
+    trave.render("main", {TenTrang: "Main Chinh !!!"} );
 });
 
 router.get( "/home" , (yeucau, trave) => {
     trave.render("home", {TenTrang: "Home Nha !!!"});
 });
 
-router.get( "/about" , (yeucau, trave) => {
-    trave.render("about", {TenTrang: "About Nha !!!"});
-});
 
 //--- Add middleware
 //const session = express.session();
 appServer.use(bodyParser.json());
-// không được copy thiếu nữa
 appServer.use(bodyParser.urlencoded({ extended: true }));
 //appServer.use(session({secret: "id-session-Mr.Tu"​}));
 
@@ -92,21 +70,6 @@ appServer.use("/products", ProductRouter);
 
 const LoginRouter = require("./controller/loginController").LoginRouter;
 appServer.use("/login", LoginRouter);
-
-const PCRouter = require("./controller/PCController").PCRouter;
-appServer.use("/PC", PCRouter);
-
-const CategoryRouter = require("./controller/CategoryController").CategoryRouter;
-appServer.use("/Category", CategoryRouter);
-
-const UserRouter = require("./controller/UserController").UserRouter;
-appServer.use("/User", UserRouter);
-
-const AdminRouter = require("./controller/AdminController").AdminRouter;
-appServer.use("/Admin", AdminRouter);
-
-
-
 
 // ----------- RUN / Launching !!! 
 appServer.listen( PORT );
